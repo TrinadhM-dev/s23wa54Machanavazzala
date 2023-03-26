e9af0a01ba3a7a67af0ebd734b1e7abb4aae2cbf
TrinadhM-dev <79310517+TrinadhM-dev@users.noreply.github.com>
2023-03-26 12:54:37 -0500

    add logic

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+let sum = 0;
+let more = 0;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+
+   more = more+1;
+   sum = more+sum;
+  res.send(`sum is ${sum}`);
 });
 
 module.exports = router;
+
