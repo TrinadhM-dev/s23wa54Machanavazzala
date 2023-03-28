91969f69a786d85a78e259669fdee16ca2ee7ced
TrinadhM-dev <79310517+TrinadhM-dev@users.noreply.github.com>
2023-03-28 15:46:02 -0500

    Bonus Code

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var index = require('./routes/index');
 var usersRouter = require('./routes/users');
 var myDataRouter = require('./routes/mydata');
+var computeRouter = require('./routes/compute');
 var app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', usersRouter);
 app.use('/mydata',myDataRouter);
+app.use('/compute',computeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/compute.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page.*/
+let x;
+let y;
+router.get('/',function(req,res,next){
+    if(req.query.x == undefined){
+        //S559254 4 has hypot,ceil and clz32
+        x = Math.floor(Math.random()*5);
+        y = Math.floor(x);
+        let result = Math.hypot(x,y );
+        let result2 = Math.ceil(x);
+        let result3 = Math.clz32(x);
+        res.send(`Math.hypot() of ${x} is ${result}
+        Math.ceil() of ${x} is ${result2}
+        Math.clz32() of ${x} is ${result3}
+        `);
+    }
+    else {
+        x = req.query.x;
+        let result = Math.hypot(x,y);
+        let result2 = Math.ceil(x);
+        let result3 = Math.clz32(x);
+        res.send(`Math.hypot() of ${x}is ${result}
+        Math.ceil() of ${x} is ${result2}
+        Math.clz32() of ${x} is ${result3}
+        `);
+    }
+})
+module.exports = router;
