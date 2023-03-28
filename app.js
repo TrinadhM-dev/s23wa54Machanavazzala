fcebff4b3e49b5549cc772c57796fce925e19e2f
TrinadhM-dev <79310517+TrinadhM-dev@users.noreply.github.com>
2023-03-28 14:18:19 -0500

    Code revert for index

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var mydataRouter = require('./routes/mydata');
+var index = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', mydataRouter);
+app.use('/', index);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
